d732d775b024be95fa6dcb7eeef719a26de3c651
unknown <lokanath598@gmail.com>
2020-09-18 21:52:06 +0530

    rebuild commit

---
+++ build/precache-manifest.dec3c56b4fe764cabb34d36889ac4278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0916c5e8cc9224667c87ef4da7c3629b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69bfcfb5b051025d5034",
+    "url": "/static/css/main.a6e9dfe1.chunk.css"
+  },
+  {
+    "revision": "14b0b53e946d0a76b0c0",
+    "url": "/static/js/2.abb86b97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.abb86b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69bfcfb5b051025d5034",
+    "url": "/static/js/main.7c51b3e5.chunk.js"
+  },
+  {
+    "revision": "441cfea73f775386a3f5",
+    "url": "/static/js/runtime-main.02030f09.js"
+  }
+]);
